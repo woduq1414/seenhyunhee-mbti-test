169fc14ae345a9c43adb2b05700961e9da9ac154
정재엽 <jjy37777@naver.com>
2022-04-03 21:05:24 +0900

    change test content

--- main.js
+++ main.js
@@ -38,14 +38,63 @@ function initTest() {
 
 initTest();
 
+
+window.onpopstate = function (e) {
+
+    if (location.hash === "") {
+
+        initTest();
+        retest();
+    }
+}
+
+
+function swapNode(node1, node2) {
+    const afterNode2 = node2.nextElementSibling;
+    const parent = node2.parentNode;
+    node1.replaceWith(node2);
+    parent.insertBefore(node1, afterNode2);
+}
+
+function shuffleOption() {
+    let ul_q = document.querySelector("ul.q");
+    let li_list = ul_q.querySelectorAll("li");
+    console.log(li_list);
+    for (let i = 1; i <= 12; i++) {
+        if (Math.random() > 0.5) {
+            let li = li_list[i];
+            let btns = li.querySelectorAll("button");
+            let bt1 = btns[0];
+            let bt2 = btns[1];
+
+            bt1.classList.remove("A");
+            bt1.classList.add("B");
+            bt2.classList.remove("B");
+            bt2.classList.add("A");
+
+
+            swapNode(bt1, bt2);
+        }
+
+
+    }
+}
+
+
+
+
+
+
 //youtube API 불러오는 부분
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
+var player = undefined;
+
 function onYouTubeIframeAPIReady() {
+    console.log("EE");
     player = new YT.Player('player', {
         //width&height를 설정할 수 있으나, 따로 css영역으로 뺐다.
         events: {
@@ -262,9 +311,9 @@ function move() {
 
 
 function retest() {
+    shuffleOption();
 
-
-
+    location.hash = "";
     clearInterval(timer);
     timer = undefined;
 
@@ -330,149 +379,237 @@ function showLyricHighlight() {
 
 $(function () {
 
-    playpauseBtn = document.querySelector('#play-btn');
-    progress = document.querySelector('.progress');
-    sliders = document.querySelectorAll('.slider');
-    // let i = 0
-    // for (let x in mbtiData["mbti"]) {
-    //     console.log(x);
-    //     if (i >= 0 && i < 16) {
-    //         $("body").append(`
-    //         <iframe width="560" height="315" src="https://www.youtube.com/embed/${mbtiData["mbti"][x]["videoCode"]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    //         `)
-    //     }
 
-    //     i += 1;
-    // }
+    $("ul.q").hide();
+    $(".testProgress").hide();
+    $(".progressNum").hide();
+    shuffleOption();
+    (function () {
+        var nTimer = setInterval(function () {
 
+            if (player !== undefined) {
+                clearInterval(nTimer);
 
+                setTimeout(function () {
+                    playpauseBtn = document.querySelector('#play-btn');
+                    progress = document.querySelector('.progress');
+                    sliders = document.querySelectorAll('.slider');
 
-    $("#mbtiResult").hide();
+                    sliders.forEach(slider => {
+                        let pin = slider.querySelector('.pin');
+                        slider.addEventListener('click', window[pin.dataset.method]);
+                    });
+                    // let i = 0
+                    // for (let x in mbtiData["mbti"]) {
+                    //     console.log(x);
+                    //     if (i >= 0 && i < 16) {
+                    //         $("body").append(`
+                    //         <iframe width="560" height="315" src="https://www.youtube.com/embed/${mbtiData["mbti"][x]["videoCode"]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    //         `)
+                    //     }
 
-    // $(".q").fadeOut(1000);
-    // $(".testProgress").fadeOut(1000);
-    // $(".progressNum").fadeOut(1000);
-    // $(".n").fadeIn(1000);
-
-
-    $("#goListPageButton").on("click", function(){
-        
-    });
-
-
-    $(".q button").on("click", function () {
-
-        // index = $(this).parent("li").index();
-        // console.log(index);
+                    //     i += 1;
+                    // }
 
 
-        if ($(this).hasClass("e")) {
-            e++;
-        } else if ($(this).hasClass("s")) {
-            s++;
-        } else if ($(this).hasClass("t")) {
-            t++;
-        } else if ($(this).hasClass("j")) {
-            j++;
-        }
-        if (prog <= 12) {
-            $(".q li").removeClass("now");
-            $("li").eq(prog - 1).addClass("hiddenLi");
-            $("li").eq(prog - 1 + 1).addClass("now");
-            $(".bar").attr('style', 'width: calc(100/12*' + prog + '%)');
-            $(".progressNum").children("span").text(prog);
 
-            if (prog <= 4) {
-                $(".bar").css({ "background-color": "rgb(115, 116, 31, 0.87)" });
-            } else if (prog <= 8) {
-                $(".bar").css({ "background-color": "rgb(116, 34, 106, 0.87)" });
-            } else {
-                $(".bar").css({ "background-color": "rgb(28, 104, 135, 0.87)" });
+                    $("#mbtiResult").hide();
+
+                    // $(".q").fadeOut(1000);
+                    // $(".testProgress").fadeOut(1000);
+                    // $(".progressNum").fadeOut(1000);
+                    // $(".n").fadeIn(1000);
+
+
+                    $("#goListPageButton").on("click", function () {
+
+                    });
+
+
+                    $(".q button").on("click", function () {
+
+                        // index = $(this).parent("li").index();
+                        // console.log(index);
+
+
+                        if ($(this).hasClass("e")) {
+                            e++;
+                        } else if ($(this).hasClass("s")) {
+                            s++;
+                        } else if ($(this).hasClass("t")) {
+                            t++;
+                        } else if ($(this).hasClass("j")) {
+                            j++;
+                        }
+                        if (prog <= 12) {
+                            $(".q li").removeClass("now");
+                            $("li").eq(prog - 1).addClass("hiddenLi");
+                            $("li").eq(prog - 1 + 1).addClass("now");
+                            $(".bar").attr('style', 'width: calc(100/12*' + prog + '%)');
+                            $(".progressNum").children("span").text(prog);
+
+                            if (prog <= 4) {
+                                $(".bar").css({ "background-color": "rgb(115, 116, 31, 0.87)" });
+                            } else if (prog <= 8) {
+                                $(".bar").css({ "background-color": "rgb(116, 34, 106, 0.87)" });
+                            } else {
+                                $(".bar").css({ "background-color": "rgb(28, 104, 135, 0.87)" });
+                            }
+
+                            prog++;
+                        } else {
+                            $(".q").fadeOut(1000);
+                            $(".testProgress").fadeOut(1000);
+                            $(".progressNum").fadeOut(1000);
+                            $(".n").fadeIn(1000);
+                        }
+                        console.log(e, s, t, j);
+                    });
+                    $(".n button").on("click", function () {
+                        (e >= 2) ? mbti += "E" : mbti += "I";
+                        (s >= 2) ? mbti += "S" : mbti += "N";
+                        (t >= 2) ? mbti += "T" : mbti += "F";
+                        (j >= 2) ? mbti += "J" : mbti += "P";
+
+                        // mbti = "ENTP";
+
+                        console.log(mbti);
+                        $(".n").fadeOut(1000);
+
+
+
+
+                        var state = { "mbti": mbti };
+                        var url = location.origin + '#' + mbti;
+                        history.pushState(state, null, url);
+
+                        renderMbtiResult(mbti);
+
+
+                        // var height = $target.outerHeight();
+                        // $(".a").height(height + 80);
+                    });
+
+
+                    if (location.hash.includes("#")) {
+                        let mbti = location.hash.split("#")[1].toUpperCase();
+                        let isMbtiExist = mbtiData["mbti"].hasOwnProperty(mbti);
+                        console.log(isMbtiExist, mbti);
+                        if (isMbtiExist) {
+                            location.hash = "#" + mbti;
+                            var state = { "mbti": mbti };
+                            var url = location.origin + '#' + mbti;
+                            history.replaceState(state, null, url);
+
+
+                            $(".q").fadeOut(1000);
+                            $(".testProgress").fadeOut(1000);
+                            $(".progressNum").fadeOut(1000);
+                            // $(".a").fadeIn(1000);
+
+                            $(".loader").fadeOut(500);
+
+                            renderMbtiResult(mbti);
+
+                            $("ul.q").removeClass("hidden");
+                            $(".testProgress").removeClass("hidden");
+                            $(".progressNum").removeClass("hidden");
+
+
+
+                            return;
+                        } else {
+
+
+                        }
+
+                    }
+                    location.hash = "";
+                    var state = { "mbti": undefined };
+                    var url = location.origin;
+                    history.replaceState(state, null, url);
+                    $(".loader").fadeOut(500);
+
+                    $("ul.q").removeClass("hidden");
+                    $(".testProgress").removeClass("hidden");
+                    $(".progressNum").removeClass("hidden");
+
+
+                    $("ul.q").fadeIn(1000);
+                    $(".testProgress").fadeIn(1000);
+                    $(".progressNum").fadeIn(1000);
+                }, 2000);
+
             }
-
-            prog++;
-        } else {
-            $(".q").fadeOut(1000);
-            $(".testProgress").fadeOut(1000);
-            $(".progressNum").fadeOut(1000);
-            $(".n").fadeIn(1000);
-        }
-        console.log(e, s, t, j);
-    });
-    $(".n button").on("click", function () {
-        (e >= 2) ? mbti += "E" : mbti += "I";
-        (s >= 2) ? mbti += "S" : mbti += "N";
-        (t >= 2) ? mbti += "T" : mbti += "F";
-        (j >= 2) ? mbti += "J" : mbti += "P";
-
-        // mbti = "ENTP";
-
-        console.log(mbti);
-        $(".n").fadeOut(1000);
-
-
-        $target = $("#resultWrapper");
-        targetMbtiData = mbtiData["mbti"][mbti];
-
-        console.log(targetMbtiData);
-
-        $target.find("#songTitle").text(targetMbtiData.title);
-        $target.find("#songTitleInfo").text(targetMbtiData.title);
-
-        $target.find("#songSinger").text(targetMbtiData.singer);
-        $target.find("#songSingerInfo").text(targetMbtiData.singer);
-
-        $target.find("#lyricInside").html(targetMbtiData.lyricHighlight);
-        $target.find("#lyricAll").html(targetMbtiData.lyric);
-
-        $target.find("#lyricInside").show();
-        $target.find("#lyricAll").hide();
-
-
-        $target.find("#youtubeLink").attr("href", `https://www.youtube.com/watch?v=${targetMbtiData.videoCode}`)
-        $target.find("#melonLink").attr("href", targetMbtiData.melonLink)
-
-        $target.find("#albumImage").attr("src", `/albums/${targetMbtiData.albumImage}`).one("load", function () { //fires (only once) when loaded
-            RGB = getAverageRGB(document.getElementById('albumImage'));
-            // console.log(`linear-gradient(to bottom, black, rgb(${rgb.r},${rgb.g},${rgb.b})}`);
-
-            // $("#resultWrapper").css("background", "red");
-            // console.log(rgb);
-            $("#resultWrapper").css({ "background-image": `linear-gradient(-45deg,  rgb(0,0,0) 15%, rgb(${RGB.r * 0.9},${RGB.g * 0.9},${RGB.b * 0.9}))` });
-
-            $(".progress").css({"background-color" : `rgb(${RGB.r * 1.3},${RGB.g * 1.3},${RGB.b * 1.3})`})
-
-            $(".a").fadeIn(1000);
-        })
-        $target.find("#albumImageSmall").attr("src", `/albums/${targetMbtiData.albumImage}`);
-
-
-        $("#albumImageSmall").addClass("hidden");
-        $("#equaliser").addClass("hidden");
-        $("#timeline").addClass("hidden");
-        $("#songTitleInfo").addClass("hidden");
-        $("#songSingerInfo").addClass("hidden");
-
-
-        $("#myMbti").text(mbti);
-
-        player.loadVideoById(
-            {
-                videoId: targetMbtiData.videoCode,
-                startSeconds: targetMbtiData.videoStart
-            }
-        );
-        player.pauseVideo();
+            console.log("notload")
+        }, 100);
+    })();
 
 
 
-
-
-        // var height = $target.outerHeight();
-        // $(".a").height(height + 80);
-    });
 
 });
+
+
+function renderMbtiResult(mbti) {
+    $target = $("#resultWrapper");
+    targetMbtiData = mbtiData["mbti"][mbti];
+
+    console.log(targetMbtiData);
+
+    $target.find("#songTitle").text(targetMbtiData.title);
+    $target.find("#songTitleInfo").text(targetMbtiData.title);
+
+    $target.find("#songSinger").text(targetMbtiData.singer);
+    $target.find("#songSingerInfo").text(targetMbtiData.singer);
+
+    $target.find("#lyricInside").html(targetMbtiData.lyricHighlight);
+    $target.find("#lyricAll").html(targetMbtiData.lyric);
+
+    $target.find("#lyricInside").show();
+    $target.find("#lyricAll").hide();
+
+
+    $target.find("#youtubeLink").attr("href", `https://www.youtube.com/watch?v=${targetMbtiData.videoCode}`)
+    $target.find("#melonLink").attr("href", targetMbtiData.melonLink)
+
+    $target.find("#albumImage").attr("src", `/albums/${targetMbtiData.albumImage}`).one("load", function () { //fires (only once) when loaded
+        RGB = getAverageRGB(document.getElementById('albumImage'));
+        // console.log(`linear-gradient(to bottom, black, rgb(${rgb.r},${rgb.g},${rgb.b})}`);
+
+        // $("#resultWrapper").css("background", "red");
+        // console.log(rgb);
+        $("#resultWrapper").css({ "background-image": `linear-gradient(-45deg,  rgb(0,0,0) 15%, rgb(${RGB.r * 0.9},${RGB.g * 0.9},${RGB.b * 0.9}))` });
+
+        $(".progress").css({ "background-color": `rgb(${RGB.r * 1.3},${RGB.g * 1.3},${RGB.b * 1.3})` })
+
+        $(".a").fadeIn(1000);
+    })
+    $target.find("#albumImageSmall").attr("src", `/albums/${targetMbtiData.albumImage}`);
+
+
+    $("#albumImageSmall").addClass("hidden");
+    $("#equaliser").addClass("hidden");
+    $("#timeline").addClass("hidden");
+    $("#songTitleInfo").addClass("hidden");
+    $("#songSingerInfo").addClass("hidden");
+
+
+    $("#myMbti").text(mbti);
+
+
+    console.log("player is ", player, targetMbtiData.videoCode, player.getPlayerState());
+    player.loadVideoById(
+        {
+            videoId: targetMbtiData.videoCode,
+            startSeconds: targetMbtiData.videoStart
+        }
+    );
+    player.pauseVideo();
+
+
+
+}
 
 
 //////////////////////
@@ -502,10 +639,7 @@ function getOffset(el) {
 
 
 $(function () {
-    sliders.forEach(slider => {
-        let pin = slider.querySelector('.pin');
-        slider.addEventListener('click', window[pin.dataset.method]);
-    });
+
 });
 
 
